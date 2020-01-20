1be56c669e897d67f6732349d70919f29500526b
Andreas Mueller <andreas.mueller@columbia.edu>
2020-01-20 18:59:08 -0500

    use pima diabetes for first two exercises

--- notebooks/solutions/load_pima_diabetes.py
+++ notebooks/solutions/load_pima_diabetes.py
@@ -3,15 +3,15 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 
-wine = pd.read_csv("https://www.openml.org/data/get_csv/49817/wine_quality.arff")
-print(wine.head())
+diabetes = pd.read_csv("data/pima_diabetes.csv")
+print(diabetes.head())
 
-X = wine.drop('quality', axis=1)
-y = wine.quality
+X = diabetes.drop('class', axis=1)
+y = diabetes['class']
 
 print("Dataset size: %d  number of features: %d  number of classes: %d"
       % (X.shape[0], X.shape[1], len(np.unique(y))))
 
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-pd.plotting.scatter_matrix(X, c=y, cmap='Paired', figsize=(10, 10));
+pd.plotting.scatter_matrix(X, c=y=='tested_positive', cmap='Paired', figsize=(10, 10));
